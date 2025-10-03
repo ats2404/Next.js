803e9952d3261edb1959235d08eb76ebf4dd7e9c
ATS TECHNICAL CONSUMER SERVICES <atservice445103@gmail.com>
2025-10-03 17:09:31 +0000

    https://studio-4099405683-fadf3-default-rtdb.firebaseio.com/  used URL r

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:326582718847:web:c5d7df8cd96d3bfc4f4a4a",
   "apiKey": "AIzaSyAOrWlVNZ-V93tz920--IVNJFCgMQrxYis",
   "authDomain": "studio-4099405683-fadf3.firebaseapp.com",
+  "databaseURL": "https://studio-4099405683-fadf3-default-rtdb.firebaseio.com/",
   "measurementId": "",
   "messagingSenderId": "326582718847"
 };
