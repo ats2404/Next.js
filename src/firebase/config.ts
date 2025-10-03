b4a64b1078526a3136fe914932e91c1ad63f6042
ATS TECHNICAL CONSUMER SERVICES <atservice445103@gmail.com>
2025-10-03 17:04:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4099405683-fadf3",
+  "appId": "1:326582718847:web:c5d7df8cd96d3bfc4f4a4a",
+  "apiKey": "AIzaSyAOrWlVNZ-V93tz920--IVNJFCgMQrxYis",
+  "authDomain": "studio-4099405683-fadf3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "326582718847"
+};
